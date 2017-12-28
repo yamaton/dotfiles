d6c8c72e02dd18f90ed21e5d0ec6248c003a86bc
yamato matsuoka <yamaton@gmail.com>
2017-12-27 22:07:28 -0500

    Add ripgrep, autojump, and ranger

--- aws-setup.sh
+++ aws-setup.sh
@@ -1,12 +1,20 @@
+mkdir ~/confs
 cd ~/confs
+sudo apt install -y git
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 git clone https://github.com/olivierverdier/zsh-git-prompt.git
 cp mysetting/.zshrc ~
 cp mysetting/.tmux.conf ~
 cp mysetting/.emacs ~
-sudo apt update
+sudo apt update && sudo apt full-upgrade
 sudo apt install -y zsh
-sudo apt install -y tmux emacs-nox htop
+chsh -s $(which zsh)
+sudo apt install -y tmux emacs-nox htop ranger
+
+# autojump
+sudo apt install autojump
+echo ". /usr/local/etc/profile.d/autojump.sh" >> ~/.zshrc
+
 # tldr
 sudo apt install libzip-dev libcurl4-openssl-dev  # tldr needs them
 cd ~/confs
@@ -18,3 +26,9 @@ sudo make install
 mv ~/confs/tldr-c-client/autocomplete/complete.zsh ~/.tldr.complete
 echo "source ~/.tldr.complete" >> ~/.zshrc
 
+# ripgrep
+mkdir -p ~/bin/ripgrep
+cd ~/bin
+wget https://github.com/BurntSushi/ripgrep/releases/download/0.7.1/ripgrep-0.7.1-i686-unknown-linux-musl.tar.gz
+tar xzf ripgrep-0.7.1-i686-unknown-linux-musl.tar.gz -C ripgrep
+echo "export PATH=${PATH}:~/bin/ripgrep" >> ~/.zshenv
