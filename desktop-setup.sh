bdcabccdf71f24f4aa5c02414f9a354dcb159759
yamaton <yamaton@gmail.com>
2018-05-04 15:49:14 -0400

    Add desktop-setup.sh

---
+++ desktop-setup.sh
@@ -0,0 +1,11 @@
+# miniconda
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+chmod +x ./Miniconda3-latest-Linux-x86_64.sh
+./Miniconda3-latest-Linux-x86_64.sh
+
+# misc software
+sudo apt install -y vlc gnome-tweak-tool filezilla meld cntlm
+sudo add-apt-repository universe
+sudo apt install -y fonts-firacode
+
+
