139a6c1a29db568fd1e9857d6130c5f86fa6c5d3
Yamato Matsuoka <yamaton@gmail.com>
2018-05-12 14:49:56 -0400

    Add neovim and vscode to desktop-setting.sh

--- desktop-setup.sh
+++ desktop-setup.sh
@@ -3,6 +3,17 @@ wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod +x ./Miniconda3-latest-Linux-x86_64.sh
 ./Miniconda3-latest-Linux-x86_64.sh
 
+# nvim
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt update
+sudo apt -y install neovim
+
+# vscode
+sudo add-apt-repository -y "deb https://packages.microsoft.com/repos/vscode stable main"
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EB3E94ADBE1229CF
+sudo apt update
+sudo apt -y install code
+
 # misc software
 sudo apt install -y vlc gnome-tweak-tool filezilla meld cntlm
 sudo add-apt-repository universe
