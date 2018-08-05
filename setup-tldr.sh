3fa37a47429dd175ef6e3f1da46a14259dd566ea
yamaton <yamaton@gmail.com>
2018-08-04 22:55:41 -0400

    Fix setup-tldr.sh to work as standalone

--- setup-tldr.sh
+++ setup-tldr.sh
@@ -2,13 +2,20 @@
 # Run `./setup-dldr.sh <target-dir>`
 # Then it will clone tlcr-cpp-client under <target-dir> and install it.
 #
-CONFDIR="$1"
+if [ "$#" -gt 0 ]; then
+    REPO_DIR="$1"
+else
+    REPO_DIR="$HOME"/repos
+    if [ ! -d "$REPO_DIR" ]; then
+        mkdir "$REPO_DIR"
+    fi
+fi
 sudo apt install -y libzip-dev libcurl4-openssl-dev  # tldr needs them
-cd "${CONFDIR}"
+cd "${REPO_DIR}"
 git clone https://github.com/tldr-pages/tldr-cpp-client.git tldr-cpp-client
 cd tldr-cpp-client
 ./deps.sh
 make
 sudo make install
-mv "${CONFDIR}"/tldr-cpp-client/autocomplete/complete.zsh ~/.tldr.complete
+mv "${REPO_DIR}"/tldr-cpp-client/autocomplete/complete.zsh ~/.tldr.complete
 echo "source ~/.tldr.complete" >> ~/.zshrc
