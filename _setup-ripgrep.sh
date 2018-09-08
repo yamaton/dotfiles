ebcab2eeea1fe2cdcd5fc7f5f3fb0a0735a243ce
Yamato Matsuoka <yamaton@gmail.com>
2018-09-08 03:59:18 +0000

    Install ripgrep v0.10.0

--- _setup-ripgrep.sh
+++ _setup-ripgrep.sh
@@ -1,4 +1,4 @@
 if [ ! -x "$(command -v rg)" ]; then
-    wget https://github.com/BurntSushi/ripgrep/releases/download/0.9.0/ripgrep_0.9.0_amd64.deb
-    sudo apt install ./ripgrep_0.9.0_amd64.deb
+    wget https://github.com/BurntSushi/ripgrep/releases/download/0.10.0/ripgrep_0.10.0_amd64.deb
+    sudo apt install ./ripgrep_0.10.0_amd64.deb
 fi
