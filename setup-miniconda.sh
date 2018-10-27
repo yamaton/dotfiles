f7bf8cc54886ac99b66c3643f22133312a495c59
yamaton <yamaton@gmail.com>
2018-10-27 11:56:00 -0400

    handle conda channels better

--- setup-miniconda.sh
+++ setup-miniconda.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-# miniconda
-cd ~
+BASEDIR=$(dirname $(readlink -f "$0"))
+CONFDIR="${HOME}/confs"
+
+cd "$CONFDIR"
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod +x ./Miniconda3-latest-Linux-x86_64.sh
 ./Miniconda3-latest-Linux-x86_64.sh
+. ~/miniconda3/etc/profile.d/conda.sh
+cp "${BASEDIR}/.condarc" ~
+conda update --all
+conda create -n tf -c conda-forge tensorflow opencv
+conda activate tf
+conda config --add channels conda-forge --env
