e2e0ee08e30be9c6dde1a1a3de1feb38affe9ea9
yamaton <yamaton@gmail.com>
2018-10-29 17:21:51 -0400

    download some fonts

---
+++ _download_fonts.sh
@@ -0,0 +1,2 @@
+wget https://github.com/adobe-fonts/source-code-pro/releases/download/variable-fonts/SourceCodeVariable-Italic.otf
+wget https://github.com/adobe-fonts/source-code-pro/releases/download/variable-fonts/SourceCodeVariable-Roman.otf
