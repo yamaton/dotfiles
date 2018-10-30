2c529eba3fba3b8a92449e0360617af2089c7bdf
yamaton <yamaton@gmail.com>
2018-10-29 20:22:29 -0400

    recover debian option

--- _setup-nnn.sh
+++ _setup-nnn.sh
@@ -13,28 +13,29 @@ else
     fi
 fi
 
-wget https://github.com/jarun/nnn/releases/download/v2.0/nnn_2.0-1_ubuntu18.04.amd64.deb
-sudo apt install ./nnn_2.0-1_ubuntu18.04.amd64.deb
+codename=$(lsb_release -s -c)
 
-
-# if [ "$os_str" == "Ubuntu" ]; then
-#     wget https://github.com/jarun/nnn/releases/download/v2.0/nnn_2.0-1_ubuntu18.04.amd64.deb
-#     sudo apt install ./nnn_2.0-1_ubuntu18.04.amd64.deb
-# else
-#     if [ "$os_str" == "Debian" ]; then
-#         wget https://github.com/jarun/nnn/releases/download/v2.0/nnn_2.0-1_debian9.amd64.deb
-#         sudo apt install ./nnn_2.0-1_debian9.amd64.deb
-#     else
-#         cd "${REPO_DIR}"
-#         echo "REPO_DIR: ${REPO_DIR}"
-#         if [ ! -d nnn ]; then
-#             git clone https://github.com/jarun/nnn.git
-#             cd nnn
-#         else
-#             cd nnn
-#             git pull
-#         fi
-#         make
-#         sudo make install
-#     fi
-# fi
+if [ "$codename" == "bionic" ]; then
+    echo "[INFO] getting deb for $codename"
+    wget https://github.com/jarun/nnn/releases/download/v2.0/nnn_2.0-1_ubuntu18.04.amd64.deb
+    sudo apt install ./nnn_2.0-1_ubuntu18.04.amd64.deb
+else
+    if [ "$codename" == "stretch" ]; then
+        echo "[INFO] getting deb for $codename"
+        wget https://github.com/jarun/nnn/releases/download/v2.0/nnn_2.0-1_debian9.amd64.deb
+        sudo apt install ./nnn_2.0-1_debian9.amd64.deb
+    else
+        echo "[INFO] build nnn"
+        cd "${REPO_DIR}"
+        echo "REPO_DIR: ${REPO_DIR}"
+        if [ ! -d nnn ]; then
+            git clone https://github.com/jarun/nnn.git
+            cd nnn
+        else
+            cd nnn
+            git pull
+        fi
+        make
+        sudo make install
+    fi
+fi
