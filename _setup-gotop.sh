02fc779b01db662eda0f8ac57ff785921eebc0cc
yamaton <yamaton@gmail.com>
2018-12-01 13:55:10 -0500

    Update gotop source

--- _setup-gotop.sh
+++ _setup-gotop.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-if [ ! -x "$(command -v gotop)" ]; then
+
+if [ "$1" = "-f" ] || [ ! -x "$(command -v gotop)" ]; then
     if [ "$(uname -m)" == "x86_64" ]; then
-        wget https://github.com/cjbassi/gotop/releases/download/1.5.0/gotop_1.5.0_linux_amd64.tgz
-        tar xzf gotop_1.5.0_linux_amd64.tgz
+        wget https://github.com/cjbassi/gotop/releases/download/1.5.1/gotop_1.5.1_linux_amd64.tgz
+        tar xzf gotop_1.5.1_linux_amd64.tgz
     fi
     if [ "$(uname -m)" == "armv7l" ]; then
-        wget https://github.com/cjbassi/gotop/releases/download/1.5.0/gotop_1.5.0_linux_arm7.tgz
-        tar xzf gotop_1.5.0_linux_arm7.tgz
+        wget https://github.com/cjbassi/gotop/releases/download/1.5.1/gotop_1.5.1_linux_arm7.tgz
+        tar xzf gotop_1.5.1_linux_arm7.tgz
     fi
 
-    [ ! -d ~/bin ] &&  mkdir ~/bin
+    [ ! -d ~/bin ] && mkdir ~/bin
     mv gotop ~/bin
 fi
