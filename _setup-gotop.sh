b62243d4a49b29d6c1a94d06b9dfaae5c2321258
yamaton <yamaton@gmail.com>
2018-12-18 12:56:14 -0500

    Update gotop links

--- _setup-gotop.sh
+++ _setup-gotop.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-
 if [ "$1" = "-f" ] || [ ! -x "$(command -v gotop)" ]; then
     if [ "$(uname -m)" == "x86_64" ]; then
-        wget https://github.com/cjbassi/gotop/releases/download/1.5.1/gotop_1.5.1_linux_amd64.tgz
-        tar xzf gotop_1.5.1_linux_amd64.tgz
+        wget https://github.com/cjbassi/gotop/releases/download/1.7.1/gotop_1.7.1_linux_amd64.tgz
+        tar xzf gotop_1.7.1_linux_amd64.tgz
     fi
     if [ "$(uname -m)" == "armv7l" ]; then
-        wget https://github.com/cjbassi/gotop/releases/download/1.5.1/gotop_1.5.1_linux_arm7.tgz
-        tar xzf gotop_1.5.1_linux_arm7.tgz
+        wget https://github.com/cjbassi/gotop/releases/download/1.7.1/gotop_1.7.1_linux_arm7.tgz
+        tar xzf gotop_1.7.1_linux_arm7.tgz
     fi
-
+    if [ "$(uname -m)" == "darwin" ]; then
+        wget https://github.com/cjbassi/gotop/releases/download/1.7.1/gotop_1.7.1_darwin_amd64.tgz
+        tar xzf gotop_1.7.1_darwin_amd64.tgz
+    fi
     [ ! -d ~/bin ] && mkdir ~/bin
     mv gotop ~/bin
 fi
