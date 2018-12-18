190f3a1076215b2e2c1a1beb5dbc8d3196a1b1cf
yamaton <yamaton@gmail.com>
2018-12-18 12:52:27 -0500

    Add downloading IBM Plex fonts

--- _download_fonts.sh
+++ _download_fonts.sh
@@ -1,3 +1,3 @@
 wget https://github.com/adobe-fonts/source-code-pro/releases/download/variable-fonts/SourceCodeVariable-Italic.otf
 wget https://github.com/adobe-fonts/source-code-pro/releases/download/variable-fonts/SourceCodeVariable-Roman.otf
-
+wget https://github.com/IBM/plex/releases/download/v1.2.3/OpenType.zip
