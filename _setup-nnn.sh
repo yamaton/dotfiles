bb22f49736f06ba8f9bc6d9c34e6b4382e9d3b6f
yamaton <yamaton@gmail.com>
2018-12-27 15:24:55 -0500

    add xenial deb installation in nnn

--- _setup-nnn.sh
+++ _setup-nnn.sh
@@ -15,17 +15,25 @@ if [ "$1" = "-f" ] || [ ! -x "$(command -v nnn)" ]; then
         brew install nnn
     else
         codename=$(lsb_release -s -c)
-        if [ "$codename" == "bionic" ]; then
-            echo "[INFO] getting deb for $codename"
-            wget "https://github.com/jarun/nnn/releases/download/${VER_SHORT}/nnn_${VERSION}_ubuntu18.04.amd64.deb"
-            sudo apt install "./nnn_${VERSION}_ubuntu18.04.amd64.deb"
-        elif [ "$codename" == "stretch" ]; then
-            echo "[INFO] getting deb for $codename"
-            wget "https://github.com/jarun/nnn/releases/download/${VER_SHORT}/nnn_${VERSION}_debian9.amd64.deb"
-            sudo apt install "./nnn_${VERSION}_debian9.amd64.deb"
+        case "$codename" in
+            "bionic")
+                OS="ubuntu18.04";;
+            "xenial")
+                OS="ubuntu16.04";;
+            "stretch")
+                OS="debian9";;
+            *)
+                OS="";;
+        esac
+
+        cd "${REPO_DIR}"
+        if [ ! -z "$OS" ]; then
+            echo "[INFO] getting deb file for $codename"
+            URI="https://github.com/jarun/nnn/releases/download/${VER_SHORT}/nnn_${VERSION}_${OS}.amd64.deb"
+            wget "$URI"
+            sudo apt install "./nnn_${VERSION}_${OS}.amd64.deb"
         else
             echo "[INFO] build nnn"
-            cd "${REPO_DIR}"
             echo "REPO_DIR: ${REPO_DIR}"
             if [ ! -d nnn ]; then
                 git clone https://github.com/jarun/nnn.git
