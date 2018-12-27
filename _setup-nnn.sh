edb14e632bfb6215511585ddf0ab13d35502350d
yamato matsuoka <yamaton@gmail.com>
2018-12-27 13:52:43 -0500

    cleanup nnn installation

--- _setup-nnn.sh
+++ _setup-nnn.sh
@@ -4,6 +4,9 @@
 # Run `./_setup-nnn.sh <repo-root>`
 # Then it will clone nnn under <repo-root> and install it.
 
+VERSION="2.1-1"
+VER_SHORT="v${VERSION%-*}"
+
 if [ "$#" -gt 0 ]; then
     REPO_DIR="$1"
 else
@@ -13,29 +16,32 @@ else
     fi
 fi
 
-codename=$(lsb_release -s -c)
-
-if [ "$codename" == "bionic" ]; then
-    echo "[INFO] getting deb for $codename"
-    wget https://github.com/jarun/nnn/releases/download/v2.1/nnn_2.1-1_ubuntu18.04.amd64.deb
-    sudo apt install ./nnn_2.1-1_ubuntu18.04.amd64.deb
-else
-    if [ "$codename" == "stretch" ]; then
-        echo "[INFO] getting deb for $codename"
-        wget https://github.com/jarun/nnn/releases/download/v2.1/nnn_2.1-1_debian9.amd64.deb
-        sudo apt install ./nnn_2.1-1_debian9.amd64.deb
+if [ "$1" = "-f" ] || [ ! -x "$(command -v nnn)" ]; then
+    if [ $(uname -s) == "Darwin"]; then
+        brew install nnn
     else
-        echo "[INFO] build nnn"
-        cd "${REPO_DIR}"
-        echo "REPO_DIR: ${REPO_DIR}"
-        if [ ! -d nnn ]; then
-            git clone https://github.com/jarun/nnn.git
-            cd nnn
+        codename=$(lsb_release -s -c)
+        if [ "$codename" == "bionic" ]; then
+            echo "[INFO] getting deb for $codename"
+            wget "https://github.com/jarun/nnn/releases/download/${VER_SHORT}/nnn_${VERSION}_ubuntu18.04.amd64.deb"
+            sudo apt install "./nnn_${VERSION}_ubuntu18.04.amd64.deb"
+        elif [ "$codename" == "stretch" ]; then
+            echo "[INFO] getting deb for $codename"
+            wget "https://github.com/jarun/nnn/releases/download/${VER_SHORT}/nnn_${VERSION}_debian9.amd64.deb"
+            sudo apt install "./nnn_${VERSION}_debian9.amd64.deb"
         else
-            cd nnn
-            git pull
+            echo "[INFO] build nnn"
+            cd "${REPO_DIR}"
+            echo "REPO_DIR: ${REPO_DIR}"
+            if [ ! -d nnn ]; then
+                git clone https://github.com/jarun/nnn.git
+                cd nnn
+            else
+                cd nnn
+                git pull
+            fi
+            make
+            sudo make install
         fi
-        make
-        sudo make install
     fi
 fi
