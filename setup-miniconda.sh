2050def15a4532089294c58acf1b82e9fbc5006e
yamaton <yamaton@gmail.com>
2018-12-27 16:25:43 -0500

    add dawin in miniconda installation

--- setup-miniconda.sh
+++ setup-miniconda.sh
@@ -4,12 +4,21 @@ BASEDIR=$(dirname $(readlink -f "$0"))
 CONFDIR="${HOME}/confs"
 
 cd "$CONFDIR"
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-chmod +x ./Miniconda3-latest-Linux-x86_64.sh
-./Miniconda3-latest-Linux-x86_64.sh
-. ~/miniconda3/etc/profile.d/conda.sh
+if [ $(uname -s) == "Darwin" ]; then
+    OS="MacOSX"
+else
+    OS="Linux"
+fi
+
+URI="https://repo.anaconda.com/miniconda/Miniconda3-latest-${OS}-x86_64.sh"
+wget "$URI"
+chmod +x ./Miniconda3-latest-${OS}-x86_64.sh
+./Miniconda3-latest-${OS}-x86_64.sh
+
+source ~/miniconda3/etc/profile.d/conda.sh
 cp "${BASEDIR}/.condarc" ~
+
 conda update --all
-conda create -n tf -c conda-forge tensorflow black opencv=3.3.0
+conda create -n tf -c conda-forge tensorflow opencv=3.3.0
 conda activate tf
 conda config --add channels conda-forge --env
