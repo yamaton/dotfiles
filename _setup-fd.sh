7793c28a72831da3bdab29d486410685802c0d55
yamaton <yamaton@gmail.com>
2018-12-27 14:20:12 -0500

    fix syntax error

--- _setup-fd.sh
+++ _setup-fd.sh
@@ -5,9 +5,9 @@ VERSION="v7.2.0"
 
 if [ "$1" = "-f" ] || [ ! -x "$(command -v ${CMD})" ]; then
 
-    if [ $(uname -s) == "Darwin"]; then
+    if [ $(uname -s) == "Darwin" ]; then
         brew install "${CMD}"
-    elif [ $(uname -s) == "Linux"]; then
+    elif [ $(uname -s) == "Linux" ]; then
         if [ "$(uname -m)" == "x86_64" ]; then
             curl -L "https://github.com/sharkdp/fd/releases/download/${VERSION}/fd-musl_${VERSION}_amd64.deb" | sudo apt install -
         elif [ "$(uname -m)" == "armv7l" ]; then
