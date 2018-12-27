f710f6419d04ffc5b0fa447928d73f7b6017322e
yamaton <yamaton@gmail.com>
2018-12-27 14:27:30 -0500

    cleanup fd and ripgrep installation

--- _setup-fd.sh
+++ _setup-fd.sh
@@ -9,9 +9,11 @@ if [ "$1" = "-f" ] || [ ! -x "$(command -v ${CMD})" ]; then
         brew install "${CMD}"
     elif [ $(uname -s) == "Linux" ]; then
         if [ "$(uname -m)" == "x86_64" ]; then
-            curl -L "https://github.com/sharkdp/fd/releases/download/${VERSION}/fd-musl_${VERSION}_amd64.deb" | sudo apt install -
+            URI="https://github.com/sharkdp/fd/releases/download/${VERSION}/fd-musl_${VERSION}_amd64.deb"
+            curl -L "${URI}" | sudo apt install -
         elif [ "$(uname -m)" == "armv7l" ]; then
-            curl -L "https://github.com/sharkdp/fd/releases/download/${VERSION}/fd-${VERSION}-arm-unknown-linux-gnueabihf.tar.gz" | tar xzf -
+            URI="https://github.com/sharkdp/fd/releases/download/${VERSION}/fd-${VERSION}-arm-unknown-linux-gnueabihf.tar.gz"
+            curl -L "${URI}" | tar xzf -
             [ ! -d ~/bin ] &&  mkdir ~/bin
             cp "fd-${VERSION}-arm-unknown-linux-gnueabihf/${CMD}" ~/bin
         fi
--- _setup-ripgrep.sh
+++ _setup-ripgrep.sh
@@ -7,11 +7,12 @@ if [ "$1" = "-f" ] || [ ! -x "$(command -v rg)" ]; then
         brew install ripgrep
     else
         if [ "$(uname -m)" == "x86_64" ]; then
-            wget "https://github.com/BurntSushi/ripgrep/releases/download/${VERSION}/ripgrep_${VERSION}_amd64.deb"
+            URI="https://github.com/BurntSushi/ripgrep/releases/download/${VERSION}/ripgrep_${VERSION}_amd64.deb"
+            wget "${URI}"
             sudo apt install "./ripgrep_${VERSION}_amd64.deb"
         elif [ "$(uname -m)" == "armv7l" ]; then
-            wget "https://github.com/BurntSushi/ripgrep/releases/download/${VERSION}/ripgrep-${VERSION}-arm-unknown-linux-gnueabihf.tar.gz"
-            tar xzf "./ripgrep-${VERSION}-arm-unknown-linux-gnueabihf.tar.gz"
+            URI="https://github.com/BurntSushi/ripgrep/releases/download/${VERSION}/ripgrep-${VERSION}-arm-unknown-linux-gnueabihf.tar.gz"
+            curl -L "${URI}" | tar xzf -
             cp "ripgrep-${VERSION}-arm-unknown-linux-gnueabihf/rg" ~/bin
         fi
     fi
