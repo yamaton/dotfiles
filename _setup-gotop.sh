131d67d66b2de8f60fc86655a4774c207425b89c
yamato matsuoka <yamaton@gmail.com>
2018-12-27 13:52:33 -0500

    cleanup gotop installation

--- _setup-gotop.sh
+++ _setup-gotop.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-if [ "$1" = "-f" ] || [ ! -x "$(command -v gotop)" ]; then
+CMD="gotop"
+VERSION="1.7.1"
+
+if [ "$1" = "-f" ] || [ ! -x "$(command -v ${CMD})" ]; then
     if [ "$(uname -m)" == "x86_64" ]; then
-        wget https://github.com/cjbassi/gotop/releases/download/1.7.1/gotop_1.7.1_linux_amd64.tgz
-        tar xzf gotop_1.7.1_linux_amd64.tgz
+        ARCH="amd64"
+    elif [ "$(uname -m)" == "armv7l" ]; then
+        ARCH="arm7"
     fi
-    if [ "$(uname -m)" == "armv7l" ]; then
-        wget https://github.com/cjbassi/gotop/releases/download/1.7.1/gotop_1.7.1_linux_arm7.tgz
-        tar xzf gotop_1.7.1_linux_arm7.tgz
-    fi
-    if [ "$(uname -m)" == "darwin" ]; then
-        wget https://github.com/cjbassi/gotop/releases/download/1.7.1/gotop_1.7.1_darwin_amd64.tgz
-        tar xzf gotop_1.7.1_darwin_amd64.tgz
-    fi
+
+    OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+    URI="https://github.com/cjbassi/${CMD}/releases/download/${VERSION}/${CMD}_${VERSION}_${OS}_${ARCH}.tgz"
+    curl -L "${URI}" | tar xzf -
     [ ! -d ~/bin ] && mkdir ~/bin
-    mv gotop ~/bin
+    mv "${CMD}" ~/bin
 fi
