79c0ff1bfb70bd95d98095869146560fb0061eb1
yamato matsuoka <yamaton@gmail.com>
2018-12-27 13:53:45 -0500

    cleanup tldr installation

--- _setup-tldr.sh
+++ _setup-tldr.sh
@@ -4,26 +4,32 @@
 # Run `./_setup-tldr.sh <target-dir>`
 # Then it will clone tlcr-cpp-client under <target-dir> and install it.
 
+CMD="tldr"
+
 if [ "$#" -gt 0 ]; then
     REPO_DIR="$1"
 else
-    REPO_DIR="$HOME"/confs
-    if [ ! -d "$REPO_DIR" ]; then
-        mkdir "$REPO_DIR"
+    REPO_DIR="${HOME}/confs"
+    [ ! -d "$REPO_DIR" ] && mkdir "${REPO_DIR}"
+fi
+
+if [ "$1" = "-f" ] || [ ! -x "$(command -v ${CMD})" ]; then
+    if [ $(uname -s) == "Darwin"]; then
+        brew install "${CMD}"
+    elif [ $(uname -s) == "Linux"]; then
+        sudo apt install -y libzip-dev libcurl4-openssl-dev  # tldr needs them
+        cd "${REPO_DIR}"
+        if [ ! -d tldr-cpp-client ]; then
+            git clone https://github.com/tldr-pages/tldr-cpp-client.git
+            cd tldr-cpp-client
+        else
+            cd tldr-cpp-client
+            git pull
+        fi
+        ./deps.sh
+        make
+        sudo make install
     fi
-fi
 
-sudo apt install -y libzip-dev libcurl4-openssl-dev  # tldr needs them
-cd "${REPO_DIR}"
-if [ ! -d tldr-cpp-client ]; then
-    git clone https://github.com/tldr-pages/tldr-cpp-client.git
-    cd tldr-cpp-client
-else
-    cd tldr-cpp-client
-    git pull
+    cp autocomplete/complete.zsh ~/.tldr.complete
 fi
-./deps.sh
-make
-sudo make install
-cp autocomplete/complete.zsh ~/.tldr.complete
-
