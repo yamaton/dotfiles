5d3afa8b0625cf5ddfbc784979e14d23d4aedd2b
yamaton <yamaton@gmail.com>
2019-03-10 16:32:15 -0400

    Fix xsv installation for darwin and rpi

--- _setup-xsv.sh
+++ _setup-xsv.sh
@@ -5,13 +5,15 @@ VERSION="0.13.0"
 
 if [ "$1" = "-f" ] || [ ! -x "$(command -v ${CMD})" ]; then
     if [ "$(uname -s)" == "Darwin" ]; then
-        brew install ${CMD}:
-    elif [ "$(uname -s)" == "Linux" ]; then
+        brew install ${CMD}
+    elif [ "$(uname -s)" == "Linux" ] && [ "$(uname -m)" == "x86_64" ]; then
         OS=$(uname -s | tr '[:upper:]' '[:lower:]')
         ARCH=$(uname -m | tr '[:upper:]' '[:lower:]')
         URI="https://github.com/BurntSushi/xsv/releases/download/${VERSION}/${CMD}-${VERSION}-${ARCH}-unknown-${OS}-musl.tar.gz"
         curl -L "${URI}" | tar xzf -
         [ ! -d ~/bin ] &&  mkdir ~/bin
         mv ${CMD} ~/bin
+    else
+	echo "[WARNING] Not installing ${CMD}. Build it oneself."
     fi
 fi
