0363dc01ff6febde16731087be62ac573f329df7
yamato matsuoka <yamaton@gmail.com>
2019-06-30 17:22:34 -0400

    Update dotnet core download link

--- setup-dotnet.sh
+++ setup-dotnet.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 mkdir dotnet && cd dotnet
-wget https://download.visualstudio.microsoft.com/download/pr/7e4b403c-34b3-4b3e-807c-d064a7857fe8/95c738f08e163f27867e38c602a433a1/dotnet-sdk-3.0.100-preview5-011568-linux-x64.tar.gz
-tar xzf dotnet-sdk-3.0.100-preview5-011568-linux-x64.tar.gz
-rm -f dotnet-sdk-3.0.100-preview5-011568-linux-x64.tar.gz
+wget https://download.visualstudio.microsoft.com/download/pr/72ce4d40-9063-4a2e-a962-0bf2574f75d1/5463bb92cff4f9c76935838d1efbc757/dotnet-sdk-3.0.100-preview6-012264-linux-x64.tar.gz
+tar xzf dotnet-sdk-3.0.100-preview6-012264-linux-x64.tar.gz 
+rm -f dotnet-sdk-3.0.100-preview6-012264-linux-x64.tar.gz 
 cd ..
+rm -rf ~/dotnet
 mv dotnet ~
