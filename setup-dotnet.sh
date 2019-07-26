148519aff69c1acfc0ec64bf780aefe73a395119
yamaton <yamaton@gmail.com>
2019-07-26 09:31:29 -0400

    Update dotnet

--- setup-dotnet.sh
+++ setup-dotnet.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-mkdir dotnet && cd dotnet
-wget https://download.visualstudio.microsoft.com/download/pr/72ce4d40-9063-4a2e-a962-0bf2574f75d1/5463bb92cff4f9c76935838d1efbc757/dotnet-sdk-3.0.100-preview6-012264-linux-x64.tar.gz
-tar xzf dotnet-sdk-3.0.100-preview6-012264-linux-x64.tar.gz 
-rm -f dotnet-sdk-3.0.100-preview6-012264-linux-x64.tar.gz 
+rm -rf dotnet && mkdir dotnet && cd dotnet
+wget "https://download.visualstudio.microsoft.com/download/pr/c624c5d6-0e9c-4dd9-9506-6b197ef44dc8/ad61b332f3abcc7dec3a49434e4766e1/dotnet-sdk-3.0.100-preview7-012821-linux-x64.tar.gz"
+tar xzf "dotnet-sdk-3.0.100-preview7-012821-linux-x64.tar.gz"
+rm -f "dotnet-sdk-3.0.100-preview7-012821-linux-x64.tar.gz"
 cd ..
 rm -rf ~/dotnet
 mv dotnet ~
