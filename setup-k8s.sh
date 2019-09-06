819b1d43af364120f8a7cee60471c7be47b609e1
yamaton <yamaton@gmail.com>
2019-09-06 00:12:26 -0400

    Add k8s setup (minikube)

---
+++ setup-k8s.sh
@@ -0,0 +1,19 @@
+# https://kubernetes.io/docs/tasks/tools/install-kubectl/#install-kubectl-on-linux
+
+# kubectl
+if [ "$1" = "-f" ] || [ ! -x "$(command -v kubectl)" ]; then
+    if [ -x $(command -v apt) ]; then
+        sudo apt update && sudo apt install -y apt-transport-https
+        curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+        echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+        sudo apt update
+        sudo apt install -y kubectl
+    elif [ -x $(command -v brew) ]; then
+        brew install kubernetes-cli
+    fi
+fi
+
+# minikube
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
+&& chmod +x minikube
+sudo install minikube /usr/local/bin
