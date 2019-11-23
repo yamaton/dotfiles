73bf657c8d6ea3caa36a56c7b52e96a7b45d515b
yamaton <yamaton@gmail.com>
2019-11-23 01:13:54 -0500

    Update dotnet 3.1 preview

--- setup-dotnet.sh
+++ setup-dotnet.sh
@@ -10,7 +10,7 @@ if [ -d dotnet ]; then
 fi
 
 mkdir dotnet && cd dotnet
-URL="https://download.visualstudio.microsoft.com/download/pr/a3cc3d8a-226d-4306-a61b-a5446fdb72ef/604e029047aec0229545e8c397a14ddb/dotnet-sdk-3.1.100-preview1-014459-linux-x64.tar.gz"
+URL="https://download.visualstudio.microsoft.com/download/pr/941853c3-98c6-44ff-b11f-3892e4f91814/14e8f22c7a1d95dd6fe9a53296d19073/dotnet-sdk-3.1.100-preview3-014645-linux-x64.tar.gz"
 wget $URL
 FILENAME=$(basename $URL)
 tar xzf "$FILENAME"
