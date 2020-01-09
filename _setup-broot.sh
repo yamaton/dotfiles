f33392ce2e88727fa5dfbef3b5099f5a139baaea
yamaton <yamaton@gmail.com>
2020-01-09 10:41:44 -0500

    Add broot installation

---
+++ _setup-broot.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+NAME=broot
+CMD=br
+VERSION="0.11.3"
+if [ "$1" = "-f" ] || [ ! -x "$(command -v ${CMD})" ]; then
+    if [ "$(uname -s)" == "Darwin" ]; then
+        brew install "$NAME"
+    elif [ -x "$(command -v apt)" ]; then
+        if [ "$(uname -m)" == "x86_64" ]; then
+	    URI="https://github.com/Canop/$NAME/releases/download/v$VERSION/$NAME"
+	    mkdir -p ~/bin && cd ~/bin
+        wget -N "$URI"
+        chmod +x ./"$NAME"
+	    ./"$NAME" --install
+        fi
+    fi
+fi
