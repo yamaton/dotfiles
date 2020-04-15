6193b0d218492880203a9bf5445d8be3e32e40d5
yamaton <yamaton@gmail.com>
2020-04-15 01:24:25 -0400

    Fix docker installation

--- setup-docker.sh
+++ setup-docker.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [ "$1" = "-f" ] || [ ! -x "$(command -v docker)" ]; then
+if [ ! -x "$(command -v docker)" ] || [ "$1" = "-f" ]; then
     if [ -x "$(command -v apt)" ]; then
         echo "---- Install docker ----"
         sudo apt-get install \
@@ -12,18 +12,29 @@ if [ "$1" = "-f" ] || [ ! -x "$(command -v docker)" ]; then
         sudo apt install -y apt-transport-https ca-certificates curl software-properties-common gnupg-agent
         OS=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
         curl -fsSL "https://download.docker.com/linux/$OS/gpg" | sudo apt-key add -
+
         sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/$OS $(lsb_release -cs) stable"
         sudo apt-key fingerprint 0EBFCD88
-        sudo apt install -y docker-ce
+        sudo apt update
+        sudo apt install docker-ce docker-ce-cli containerd.io
         sudo usermod -aG docker "$USER"
-        docker -v
+
+        echo "================================================"
+        echo "   logout once to call docker without sudo "
+        echo "================================================"
+        sudo docker -v
     fi
+else
+    echo "docker is already available"
+    docker -v
 fi
 
 
-COMPOSE_VERSION=1.25.0
+COMPOSE_VERSION=1.25.5
 if [ "$1" = "-f" ] || [ ! -x "$(command -v docker-compose)" ]; then
     echo "---- Install docker-compose ----"
     sudo curl -L "https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
+    echo "Installed docker-compose"
+    docker-compose -v
 fi
