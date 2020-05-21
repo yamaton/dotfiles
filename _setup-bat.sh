46a469794ef525477b5ffc55af24edbed6800c9d
yamaton <yamaton@gmail.com>
2020-05-21 19:05:45 -0400

    Replace bat deb package with musl one

--- _setup-bat.sh
+++ _setup-bat.sh
@@ -19,7 +19,7 @@ if [ "$1" = "-f" ] || [ ! -x "$(command -v ${CMD})" ]; then
         brew install "$CMD"
     elif [ -x "$(command -v apt)" ]; then
         if [ "$(uname -m)" == "x86_64" ]; then
-            URI="https://github.com/sharkdp/bat/releases/download/v${VERSION}/bat_${VERSION}_amd64.deb"
+            URI="https://github.com/sharkdp/bat/releases/download/v${VERSION}/bat-musl_${VERSION}_amd64.deb"
 
         elif [ "$(uname -m)" == "armv7l" ]; then
             URI="https://github.com/sharkdp/bat/releases/download/v${VERSION}/bat_${VERSION}_armhf.deb"
