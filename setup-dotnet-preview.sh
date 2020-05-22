2acb6c9de64309a0f1fd247ff8121783323f07cd
yamaton <yamaton@gmail.com>
2020-05-22 00:42:09 -0400

    Adapt .Net 5 installation for Pop!_OS

--- setup-dotnet-preview.sh
+++ setup-dotnet-preview.sh
@@ -4,6 +4,7 @@ MYDIR=$HOME/dotnet_install
 
 mkdir -p "$MYDIR" && cd "$MYDIR"
 curl -H 'Cache-Control: no-cache' -L https://aka.ms/install-dotnet-preview -o install-dotnet-preview.sh
+sed -i 's/*"Ubuntu 20.04"*/*"Ubuntu 20.04"* | *"Pop!_OS 20.04"*/' install-dotnet-preview.sh
 sudo bash install-dotnet-preview.sh
 cd ..
 rm -rf $MYDIR
