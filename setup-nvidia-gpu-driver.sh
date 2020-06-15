117bdf67e3a98b19eda80f036290d764c2b3c8c3
yamaton <yamaton@gmail.com>
2020-06-15 12:02:28 -0400

    Update nvidia gpu installation by removing cuda repo
    nvidia also sets up machine-learning repo, and this
    repo alone works better, it seems.

--- setup-nvidia-gpu-driver.sh
+++ setup-nvidia-gpu-driver.sh
@@ -4,12 +4,12 @@
 # https://developer.nvidia.com/cuda-downloads?target_os=Linux&target_arch=x86_64&target_distro=Ubuntu&target_version=1804&target_type=debnetwork
 # https://qiita.com/ksasaki/items/b20a785e1a0f610efa08
 
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
-sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
-sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
-sudo apt-get update
-sudo apt-get install -y cuda-drivers
+# wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
+# sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+# sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+# sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
+# sudo apt-get update
+# sudo apt-get install -y cuda-drivers
 # sudo apt-get -y install cuda
 
 
