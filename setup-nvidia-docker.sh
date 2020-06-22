f17370f7044109ec82552aaeb649d53137cf8e69
yamaton <yamaton@gmail.com>
2020-06-22 08:44:56 -0400

    Update nvidia gpu setups

--- setup-nvidia-docker.sh
+++ setup-nvidia-docker.sh
@@ -2,7 +2,8 @@
 
 ./setup-docker.sh
 
-if [ -x "$(command -v apt)" ]; then
+if [[ "$(uname -s)" == 'Linux' ]] && [ -x "$(command -v apt)" ]; then
+    # https://github.com/NVIDIA/nvidia-docker
     distribution=$(. /etc/os-release;echo "$ID""$VERSION_ID")
     curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
     curl -s -L https://nvidia.github.io/nvidia-docker/"$distribution"/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
--- setup-nvidia-gpu-driver.sh
+++ setup-nvidia-gpu-driver.sh
@@ -1,27 +1,13 @@
 #!/usr/bin/env bash
 
 # reference
-# https://developer.nvidia.com/cuda-downloads?target_os=Linux&target_arch=x86_64&target_distro=Ubuntu&target_version=1804&target_type=debnetwork
+# https://developer.nvidia.com/cuda-10.2-download-archive?target_os=Linux&target_arch=x86_64&target_distro=Ubuntu&target_version=1804&target_type=debnetwork
 # https://qiita.com/ksasaki/items/b20a785e1a0f610efa08
 
-# wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
-# sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
-# sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
-# sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
-# sudo apt-get update
-# sudo apt-get install -y cuda-drivers
-# sudo apt-get -y install cuda
-
-
-## Runtime installation
-
-echo "Checking for CUDA and installing."
-# Check for CUDA and try to install.
-if ! dpkg-query -W cuda-10-0; then
-  curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
-  dpkg -i ./cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
-  apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
-  apt-get update
-  apt-get install cuda-10-0 -y
-fi
-
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
+sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
+sudo apt-get update
+sudo apt-get install -y cuda-drivers
+sudo apt-get -y install cuda
