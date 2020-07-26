1560a01c0eade32c553f352c1f46dd73e8acc94a
yamaton <yamaton@gmail.com>
2020-07-26 17:51:08 -0400

    Move nvim config to .config

--- _setup-neovim.sh
+++ _setup-neovim.sh
@@ -45,9 +45,9 @@ if [[ "$1" == "-f" ]] || [[ ! -x "$(command -v nvim)" ]] || [[ "$confirm" == [yY
     curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
     mkdir -p "$CONFIG_DIR"
-    [[ -e "$CONFIG_DIR"/init.vim ]] && mv "$CONFIG_DIR"/init.vim "$CONFIG_DIR"/init.vim.backup
-    ln -s "$BASEDIR"/init.vim "$CONFIG_DIR"
+    [[ -e "$CONFIG_DIR/init.vim" ]] && mv -f "$CONFIG_DIR"/init.vim "$CONFIG_DIR"/init.vim.backup
+    ln -s "$BASEDIR"/.config/nvim/init.vim "$CONFIG_DIR"
 
-    [[ -e ~/.vimrc ]] && mv ~/.vimrc ~/.vimrc.backup
-    ln -s "$BASEDIR"/.vimrc ~
+    [[ -e "$HOME/.vimrc" ]] && mv -f "$HOME/.vimrc" "$HOME/.vimrc.backup"
+    ln -s "$BASEDIR"/.vimrc ~/.vimrc
 fi
