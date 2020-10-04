c23349348e9d736c87d71f594d63c6b91790087b
yamaton <yamaton@gmail.com>
2020-10-04 11:29:29 -0400

    Change topgrade setup to use prebuilt binary

--- _setup-topgrade.sh
+++ _setup-topgrade.sh
@@ -19,12 +19,26 @@ fi
 if [[ "$1" == "-f" ]] || [[ ! -x "$(command -v ${CMD})" ]] || [[ "$confirm" == [yY] ]]; then
     if [[ "$(uname -s)" == "Darwin" ]]; then
         brew install "$CMD"
-    else
-        if [[ ! -x "$(command -v cargo)" ]]; then
-            BASEDIR="$(dirname "$(readlink -f "$0")")"
-            readonly BASEDIR
-            source "${BASEDIR}/setup-rust-and-cargo.sh"
+    elif [[ "$(uname -s)" == "Linux" ]]; then
+        if [[ "$(uname -m)" == "x86_64" ]]; then
+            readonly URL="https://github.com/r-darwish/topgrade/releases/download/v${VERSION}/topgrade-v${VERSION}-x86_64-unknown-linux-musl.tar.gz"
+        elif [[ "$(uname -m)" == "armv7l" ]]; then
+            readonly URL="https://github.com/r-darwish/topgrade/releases/download/v${VERSION}/topgrade-v${VERSION}-armv7-unknown-linux-gnueabihf.tar.gz"
+        else
+            if [[ ! -x "$(command -v cargo)" ]]; then
+                BASEDIR="$(dirname "$(readlink -f "$0")")"
+                readonly BASEDIR
+                source "${BASEDIR}/setup-rust-and-cargo.sh"
+            fi
+            cargo install "$CMD"
+            exit 0
         fi
-        cargo install "$CMD"
+
+        wget -N "$URL"
+        FILE="$(basename "$URL")"
+        readonly FILE
+        tar xvf "$FILE"
+        rm -f "$FILE"
+        mv -f ./topgrade "${HOME}/bin/"
     fi
 fi
