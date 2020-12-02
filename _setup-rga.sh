f3a04c33caf5460e01e6a2dba123d6ae44d5a62e
yamaton <yamaton@gmail.com>
2020-12-02 11:49:53 -0500

    Add rga setup

---
+++ _setup-rga.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+readonly REPO="phiresky/ripgrep-all"
+readonly CMD=rga
+
+VERSION="$(./get-version-github.sh "$REPO")"
+readonly VERSION
+
+if [[ -x "$(command -v $CMD)" ]]; then
+    CURRENT="$("$CMD" --version | head -1 | cut -d ' ' -f2)"
+    readonly CURRENT
+    if [[ "$VERSION" == "$CURRENT" ]]; then
+        echo "... already the latest: ${CMD} ${CURRENT}"
+    else
+        echo "${CMD} ${VERSION} is available: (current ${CMD} ${CURRENT})"
+        read -rp "Upgrade to ${CMD} ${VERSION}? (y/N): " confirm
+    fi
+fi
+
+if [[ "$1" == "-f" ]] || [[ ! -x "$(command -v $CMD)" ]] || [[ "$confirm" == [yY] ]]; then
+    if [[ "$(uname -s)" == "Darwin" ]]; then
+        brew install "$CMD"
+    elif [[ "$(uname -s)" == "Linux" ]]; then
+        if [[ "$(uname -m)" == "x86_64" ]]; then
+            arch="x86_64"
+            uri="https://github.com/phiresky/ripgrep-all/releases/download/v${VERSION}/ripgrep_all-v${VERSION}-x86_64-unknown-linux-musl.tar.gz"
+        elif [[ "$(uname -m)" == "armv7l" ]] || [[ "$(uname -m)" == "armv8" ]]; then
+            arch="arm"
+            uri="https://github.com/phiresky/ripgrep-all/releases/download/v${VERSION}/ripgrep_all-v${VERSION}-arm-unknown-linux-gnueabihf.tar.gz"
+        else
+            echo "Unsupported architecture? $(uname -s)"
+            echo "    Exiting..."
+            exit 0
+        fi
+        wget -N "$uri"
+        filename="$(basename "$uri")"
+        dir="${filename%.tar.gz}"
+        tar xvf ./"$filename"
+        cp -f "${dir}/rga" ~/bin/
+        cp -f "${dir}/rga-preproc" ~/bin/
+        rm -f ./"$filename"
+        rm -rf "${dir}"
+    fi
+fi
