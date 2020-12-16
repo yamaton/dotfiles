814a577534a538b574e238115a2aa0ac925cd246
yamaton <yamaton@gmail.com>
2020-12-16 12:36:57 -0500

    Add gitui setup

---
+++ _setup-gitui.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+readonly CMD=gitui
+
+VERSION="$(curl --silent https://formulae.brew.sh/api/formula/${CMD}.json | jq '.versions.stable' | tr -d \")"
+readonly VERSION
+
+if [[ -x "$(command -v $CMD)" ]]; then
+    CURRENT="$("$CMD" --version | cut -d ' ' -f2)"
+    readonly CURRENT
+    if [[ "$VERSION" == "$CURRENT" ]]; then
+        echo "... already the latest: ${CMD} ${CURRENT}"
+    else
+        echo "${CMD} ${VERSION} is available: (current ${CMD} ${CURRENT})"
+        read -rp "Upgrade to ${CMD} ${VERSION}? (y/N): " confirm
+    fi
+fi
+
+if [[ "$1" == "-f" ]] || [[ ! -x "$(command -v ${CMD})" ]] || [[ "$confirm" == [yY] ]]; then
+    if [[ "$(uname -s)" == "Darwin" ]]; then
+        brew install "${CMD}"
+    elif [[ "$(uname -s)" == "Linux" ]] && [[ "$(uname -m)" == "x86_64" ]]; then
+        readonly URI="https://github.com/extrawurst/gitui/releases/download/v${VERSION}/gitui-linux-musl.tar.gz"
+        wget -N "${URI}"
+        FILE="$(basename "$URI")"
+        readonly FILE
+        tar xvf "$FILE"
+        rm -f "$FILE"
+        mv ./gitui ~/bin
+    fi
+fi
