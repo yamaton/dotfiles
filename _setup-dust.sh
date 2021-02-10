bb782e4d05dbd73d034ec6ea636c788d1f69324c
yamaton <yamaton@gmail.com>
2021-02-10 13:08:03 -0500

    Add dust

---
+++ _setup-dust.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+readonly CMD=dust
+
+VERSION="$(curl --silent https://formulae.brew.sh/api/formula/${CMD}.json | jq '.versions.stable' | tr -d \")"
+readonly VERSION
+
+if [[ -x "$(command -v $CMD)" ]]; then
+    CURRENT="$("$CMD" --version | cut -d ' ' -f2)"
+    readonly CURRENT
+    if [[ "$VERSION" == "$CURRENT" ]]; then
+        echo "... already the latest: ${CMD} ${CURRENT}"
+    else
+        echo "${CMD} ${VERSION} is available: (current ${CMD} ${CURRENT})"
+        read -rp "Upgrade to ${CMD} ${VERSION}? (y/N): " confirm
+    fi
+fi
+
+if [[ "$1" == "-f" ]] || [[ ! -x "$(command -v ${CMD})" ]] || [[ "$confirm" == [yY] ]]; then
+    if [[ "$(uname -s)" == "Darwin" ]]; then
+        brew install "$CMD"
+    elif [[ "$(uname -s)" == "Linux" ]] && [[ "$(uname -m)" == "x86_64" ]]; then
+            readonly URI="https://github.com/bootandy/dust/releases/download/v${VERSION}/${CMD}-v${VERSION}-x86_64-unknown-linux-musl.tar.gz"
+            wget -N "${URI}"
+            NAME="$(basename "$URI")"
+            readonly NAME
+            tar xzf "./${NAME}"
+            rm "./${NAME}"
+            readonly NAME1="${NAME%.*.*}"
+            echo "$NAME1"
+            [[ ! -d ~/bin ]] &&  mkdir ~/bin
+            cp -f "${NAME1}/${CMD}" ~/bin/
+            rm -rf "$NAME1"
+    else
+        echo "Unsupported. Exiting..."
+        exit 0
+    fi
+fi
