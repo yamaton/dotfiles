3e2fddebae89c2c534dd493427854f93978b9dee
yamaton <yamaton@gmail.com>
2021-02-10 12:55:55 -0500

    Add grex

---
+++ _setup-grex.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+readonly CMD=grex
+
+VERSION="$(curl --silent https://formulae.brew.sh/api/formula/${CMD}.json | jq '.versions.stable' | tr -d \")"
+readonly VERSION
+
+if [[ -x "$(command -v $CMD)" ]]; then
+    CURRENT="$("$CMD" --version | cut -d ' ' -f2)"
+    readonly CURRENT
+    if [[ "$VERSION" == "$CURRENT" ]]; then
+        echo "... already the latest: ${CMD} ${CURRENT}"
+    else
+        echo "${CMD} ${VERSION} is available: (current ${CMD} ${CURRENT})"
+        read -rp "Upgrade to ${CMD} ${VERSION}? (y/N): " confirm
+    fi
+fi
+
+if [[ "$1" == "-f" ]] || [[ ! -x "$(command -v ${CMD})" ]] || [[ "$confirm" == [yY] ]]; then
+    if [[ "$(uname -s)" == "Darwin" ]]; then
+        brew install "$CMD"
+    elif [[ "$(uname -s)" == "Linux" ]] && [[ "$(uname -m)" == "x86_64" ]]; then
+            readonly URI="https://github.com/pemistahl/grex/releases/download/v${VERSION}/${CMD}-v${VERSION}-x86_64-unknown-linux-musl.tar.gz"
+            wget -N "${URI}"
+            NAME="$(basename "$URI")"
+            readonly NAME
+            tar xvf "./${NAME}"
+            rm "./${NAME}"
+            [[ ! -d ~/bin ]] && mkdir ~/bin
+            mv -f "./${CMD}" ~/bin/
+    else
+        echo "Unsupported. Exiting..."
+        exit 0
+    fi
+fi
