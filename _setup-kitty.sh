88abe74b117ef2b3d6e8a396c9dafcdfa569ce10
yamaton <yamaton@gmail.com>
2021-04-10 16:19:35 -0400

    Fix kitty desktop integration

--- _setup-kitty.sh
+++ _setup-kitty.sh
@@ -6,6 +6,7 @@ readonly VERSION
 BASEDIR="$(dirname "$(readlink -f "$0")")"
 readonly BASEDIR
 readonly KITTY_CONF_DIR=~/.config/kitty
+readonly KITTY_HOME=~/.local/kitty.app
 
 
 if [[ -x "$(command -v $CMD)" ]]; then
@@ -22,11 +23,13 @@ fi
 if [[ "$1" == "-f" ]] || [[ ! -x "$(command -v ${CMD})" ]] || [[ "$confirm" == [yY] ]]; then
     if [[ "$(uname -s)" == "Linux" ]] && [[ -x "$(command -v apt)" ]]; then
         curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
-        mkdir ~/
-        ln -sf ~/.local/kitty.app/bin/kitty ~/bin/kitty
+        mkdir -p ~/.local/bin
+        ln -sf "${KITTY_HOME}/bin/kitty" ~/.local/bin/
         [[ ! -d "${KITTY_CONF_DIR}/kitty-themes" ]] && git clone --depth 1 git@github.com:dexpota/kitty-themes.git "${KITTY_CONF_DIR}/kitty-themes"
         ln -sf "${BASEDIR}/.config/kitty/kitty.conf" "$KITTY_CONF_DIR"
+        cp "$KITTY_HOME/share/applications/kitty.desktop" ~/.local/share/applications
+        sed -i "s|Icon=kitty|Icon=$KITTY_HOME/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty.desktop
         mkdir -p ~/.terminfo/x/
-        cp ~/.local/kitty.app/share/terminfo/x/xterm-kitty ~/.terminfo/x/
+        cp "${KITTY_HOME}/share/terminfo/x/xterm-kitty" ~/.terminfo/x/
     fi
 fi
