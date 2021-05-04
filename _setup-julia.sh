1416da96a519b3e81f4a0e5621cdbb78d8588d0a
yamaton <yamaton@gmail.com>
2021-05-04 12:33:30 -0400

    Change julia to musl
    +Add man in the setup

--- _setup-julia.sh
+++ _setup-julia.sh
@@ -20,7 +20,7 @@ if [[ "$1" == "-f" ]] || [[ ! -x "$(command -v ${CMD})" ]] || [[ "$confirm" == [
     if [[ "$(uname -s)" == "Darwin" ]]; then
         brew cask install "$CMD"
     elif [[ "$(uname -s)" == "Linux" ]] && [[ "$(uname -m)" == "x86_64" ]]; then
-        readonly URI="https://julialang-s3.julialang.org/bin/linux/x64/${VERSION%.*}/julia-${VERSION}-linux-x86_64.tar.gz"
+        readonly URI="https://julialang-s3.julialang.org/bin/musl/x64/${VERSION%.*}/julia-${VERSION}-musl-x86_64.tar.gz"
         cd ~/bin || exit
         wget -N "$URI"
         FILE="$(basename "$URI")"
@@ -28,5 +28,9 @@ if [[ "$1" == "-f" ]] || [[ ! -x "$(command -v ${CMD})" ]] || [[ "$confirm" == [
         tar xvf ./"$FILE"
         rm -f ./"$FILE"
         ln -sf "${HOME}/bin/julia-${VERSION}/bin/julia" "${HOME}/bin/julia"
+
+        mkdir -p ~/.local/share/man/man1
+        cp -r "${HOME}"/bin/julia-"${VERSION}"/share/man/man1/*.1 ~/.local/share/man/man1
+        mandb ~/.local/share/man
     fi
 fi
