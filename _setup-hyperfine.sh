098b89bd510f46ad03ada6657699666cafeb4100
yamaton <yamaton@gmail.com>
2021-05-27 11:35:40 -0400

    Add hyperfine setup

---
+++ _setup-hyperfine.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+readonly CMD=hyperfine
+
+VERSION="$(curl --silent https://formulae.brew.sh/api/formula/${CMD}.json | jq '.versions.stable' | tr -d \")"
+readonly VERSION
+
+if [[ -x "$(command -v $CMD)" ]]; then
+    CURRENT="$("$CMD" --version | cut -d ' ' -f2)"
+    readonly CURRENT
+    if [[ "$VERSION" == "$CURRENT" ]]; then
+        echo "... already the latest: ${CMD} ${CURRENT}"
+    else
+        echo "${CMD} ${VERSION} is available: (current ${CMD} ${CURRENT})"
+        read -rp "Upgrade to ${CMD} ${VERSION}? (y/N): " confirm
+    fi
+fi
+
+if [[ "$1" == "-f" ]] || [[ ! -x "$(command -v ${CMD})" ]] || [[ "$confirm" == [yY] ]]; then
+    if [[ "$(uname -s)" == "Darwin" ]]; then
+        brew install "$CMD"
+    elif [[ "$(uname -s)" == "Linux" ]] && [[ -x "$(command -v apt)" ]]; then
+        if [[ "$(uname -m)" == "x86_64" ]]; then
+            readonly URI="https://github.com/sharkdp/hyperfine/releases/download/v${VERSION}/hyperfine-musl_${VERSION}_amd64.deb"
+        elif [[ "$(uname -m)" == "armv7l" ]]; then
+            readonly URI="https://github.com/sharkdp/hyperfine/releases/download/v${VERSION}/hyperfine-musl_${VERSION}_armhf.deb"
+        fi
+        wget -N "$URI"
+        FILE="$(basename "$URI")"
+        readonly FILE
+        sudo apt install ./"$FILE"
+        rm -f ./"$FILE"
+    fi
+fi
