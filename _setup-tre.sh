02368c8dd36c326ea09dd7799f323c37e8dc05f6
yamaton <yamaton@gmail.com>
2022-05-08 22:33:43 -0400

    Fix tre installation

--- _setup-tre.sh
+++ _setup-tre.sh
@@ -6,6 +6,7 @@ BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 VERSION="$("$BASEDIR"/get-version-github.sh "${AUTHOR}/${CMD}")"
 readonly VERSION
+echo $VERSION
 
 mkdirmv () {
     mkdir -p "$2" && mv -f "$1" "$2"
@@ -27,13 +28,16 @@ if [[ "$1" == "-f" ]] || [[ ! "$(command -v ${CMD})" ]] || [[ "$confirm" == [yY]
         brew install "$CMD"
     elif [[ "$(uname -s)" == "Linux" ]] && [[ "$(command -v apt)" ]]; then
         case "$(uname -m)" in
-            "x86_64")  readonly FILE="${CMD}-v${VERSION}-x86_64-unknown-linux-musl.tar.gz" ;;
+            "x86_64")  readonly URL="https://github.com/dduan/tre/releases/download/v${VERSION}/tre-v${VERSION}-x86_64-unknown-linux-musl.tar.gz" ;;
             "armv7l")  readonly FILE="${CMD}-v${VERSION}-arm-unknown-linux-gnueabihf.tar.gz" ;;
         esac
+        echo "URL=$URL"
         wget -N "$URL"
+        FILE="$(basename $URL)"
         tar -xvf "$FILE"
         mkdirmv "${CMD}" ~/.local/bin
-        mkdirmv "${CMD}.1" ~/.local/bin
+        mkdirmv "${CMD}.1" ~/.local/share/man/man1
+        mandb ~/.local/share/man
         rm -rf "$FILE"
     fi
 fi
