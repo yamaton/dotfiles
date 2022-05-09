e06ea725b93c82284efb11b89a8c6dfeb75b6443
yamaton <yamaton@gmail.com>
2022-05-08 23:19:53 -0400

    Update nnn installation

--- _setup-nnn.sh
+++ _setup-nnn.sh
@@ -25,34 +25,15 @@ mkdir -p "$REPO_DIR"
 if [[ "$1" == "-f" ]] || [[ ! "$(command -v nnn)" ]] || [[ "$confirm" == [yY] ]]; then
     if [[ "$(uname -s)" == "Darwin" ]]; then
         brew install "$CMD"
+    elif [[ "$(uname -s)" == "Linux" ]] && [[ "$(uname -m)" == "x86_64" ]]; then
+        readonly URI="https://github.com/jarun/nnn/releases/download/v${VERSION}/nnn-static-${VERSION}.x86_64.tar.gz"
+        wget -N "$URI"
+        FILE="$(basename "$URI")"
+        readonly FILE
+        tar -xvf ./"$FILE"
+        mv -f nnn-static ~/.local/bin/nnn
+        rm -f "$FILE"
     else
-        codename="$(lsb_release -s -c)"
-        case "$codename" in
-        "focal") OS="xUbuntu_20.04" ;;
-        "buster") OS="Debian_10" ;;
-        "groovy") OS="xUbuntu_20.04" ;; # works so far
-        *) OS="" ;;
-        esac
-        readonly OS
-
-        cd "${REPO_DIR}" || exit
-        if [[ "$(uname -s)" == "Linux" ]] && [[ "$(command -v apt)" ]] &&
-            [[ "$(uname -m)" == "x86_64" ]]; then
-            echo "deb http://download.opensuse.org/repositories/home:/stig124:/nnn/${OS}/ /" | sudo tee /etc/apt/sources.list.d/home:stig124:nnn.list
-            curl -fsSL "https://download.opensuse.org/repositories/home:stig124:nnn/${OS}/Release.key" | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_stig124_nnn.gpg > /dev/null
-            sudo apt update
-            sudo apt install -y nnn
-        else
-            echo "[INFO] prepare building nnn"
-            sudo apt install -y libncursesw5-dev libreadline-dev
-
-            echo "[INFO] build nnn"
-            echo "REPO_DIR: ${REPO_DIR}"
-            [[ ! -d nnn ]] && git clone https://github.com/jarun/nnn.git
-            cd nnn
-            git pull
-            make -j "$(nproc)"
-            sudo make install
-        fi
+        echo "${CMD}: Unavailable for $(uname -m)"
     fi
 fi
