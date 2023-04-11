5923ac8a98301f1693a966609d0589b8834c2cb5
yamaton <yamaton@gmail.com>
2023-04-10 20:16:44 -0400

    Add ffsend

---
+++ _setup-ffsend.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+if [[ "${TRACE-0}" == "1" ]]; then
+    set -o xtrace
+fi
+
+readonly NAME=ffsend
+readonly REPO=timvisee
+
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+readonly BASEDIR
+VERSION="$(curl --silent https://formulae.brew.sh/api/formula/${NAME}.json | jq -r '.versions.stable')"
+readonly VERSION
+
+if [[ "$(command -v $CMD)" ]]; then
+    CURRENT="$("$CMD" --version | cut -d ' ' -f2 | cut -d 'v' -f2)"
+    readonly CURRENT
+    confirm=N
+    if [[ "$VERSION" == "$CURRENT" ]]; then        echo "... already the latest: ${NAME} ${CURRENT}"
+    else
+        echo "${NAME} ${VERSION} is available: (current ${NAME} ${CURRENT})"
+        read -rp "Upgrade to ${NAME} ${VERSION}? (y/N): " confirm
+    fi
+fi
+
+if [[ "${1-}" == "-f" ]] || [[ ! "$(command -v ${CMD})" ]] || [[ "$confirm" == [yY] ]]; then
+    if [[ "$(uname -s)" == "Darwin" ]]; then
+        brew install "$NAME"
+    elif [[ "$(uname -s)" == "Linux" ]]; then
+        case "$(uname -m)" in
+            "x86_64") FILE="${NAME}-v${VERSION}-linux-x64-static" ;;
+            *) FILE="" ;;
+        esac
+        URI="https://github.com/${REPO}/${NAME}/releases/download/v${VERSION}/${FILE}"
+        wget -c -O "$NAME" "$URI"
+        chmod +x "$NAME"
+        mv "$NAME" ~/.local/bin/
+    fi
+fi
