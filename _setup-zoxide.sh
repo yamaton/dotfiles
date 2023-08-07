cb515bb0184d996d1e4df503bd1fddaf37669ee9
yamaton <yamaton@gmail.com>
2023-08-06 23:41:06 -0400

    Update zoxide setup

--- _setup-zoxide.sh
+++ _setup-zoxide.sh
@@ -13,7 +13,7 @@ VERSION="$(curl --silent https://formulae.brew.sh/api/formula/${NAME}.json | jq 
 readonly VERSION
 
 if [[ "$(command -v $NAME)" ]]; then
-    CURRENT="$("$NAME" --version | cut -d ' ' -f2 | cut -c 2-)"
+    CURRENT="$("$NAME" --version | cut -d ' ' -f2)"
     readonly CURRENT
     confirm=N
     if [[ "$VERSION" == "$CURRENT" ]]; then        echo "... already the latest: ${NAME} ${CURRENT}"
