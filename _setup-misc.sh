0abef2a2230015bb56fa6e0f612dc580f92cb569
yamaton <yamaton@gmail.com>
2023-11-29 17:38:31 -0500

    Add bash version checker

--- _setup-misc.sh
+++ _setup-misc.sh
@@ -7,6 +7,19 @@ if [[ "${TRACE-0}" == "1" ]]; then
     set -o xtrace
 fi
 
+# Get the Bash version
+bash_version=$(bash --version | head -n1 | cut -d " " -f4 | cut -d "(" -f1)
+
+# Extract major version
+major_version=${bash_version%%.*}
+
+# Check if the major version is less than or equal to 3
+if [ "$major_version" -le 3 ]; then
+    echo "Bash version 3 or lower detected. Exiting."
+    exit 1
+fi
+
+
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 readarray -t APPS < "${BASEDIR}/misc_apps.txt"
 readonly BASEDIR
--- check-updates-utils.sh
+++ check-updates-utils.sh
@@ -7,6 +7,19 @@ if [[ "${TRACE-0}" == "1" ]]; then
     set -o xtrace
 fi
 
+# Get the Bash version
+bash_version=$(bash --version | head -n1 | cut -d " " -f4 | cut -d "(" -f1)
+
+# Extract major version
+major_version=${bash_version%%.*}
+
+# Check if the major version is less than or equal to 3
+if [ "$major_version" -le 3 ]; then
+    echo "Bash version 3 or lower detected. Exiting."
+    exit 1
+fi
+
+
 BASEDIR="$(dirname "$(readlink -f "$0")")"
 # -t strips newline
 readarray -t APPLIST < "$BASEDIR/check-updates-apps.txt"
