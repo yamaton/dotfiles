45030c08bca965ffbf73cd684d56d8d9748876c0
yamaton <yamaton@gmail.com>
2024-11-10 18:27:46 -0500

    Update croc installation

--- _setup-croc.sh
+++ _setup-croc.sh
@@ -20,7 +20,8 @@ if [[ "$(command -v $CMD)" ]]; then
     CURRENT="$("$CMD" --version | cut -d ' ' -f 3 | cut -d '-' -f 1 | cut -d v -f 2)"
     readonly CURRENT
     confirm=N
-    if [[ "$VERSION" == "$CURRENT" ]]; then        echo "... already the latest: ${CMD} ${CURRENT}"
+    if [[ "$VERSION" == "$CURRENT" ]]; then
+        echo "... already the latest: ${CMD} ${CURRENT}"
     else
         echo "${CMD} ${VERSION} is available: (current ${CMD} ${CURRENT})"
         read -rp "Upgrade to ${CMD} ${VERSION}? (y/N): " confirm
@@ -31,18 +32,6 @@ if [[ "${1-}" == "-f" ]] || [[ ! "$(command -v ${CMD})" ]] || [[ "$confirm" == [
     if [[ "$(uname -s)" == "Darwin" ]]; then
         brew install "$CMD"
     elif [[ "$(uname -s)" == "Linux" ]]; then
-        case "$(uname -m)" in
-            "x86_64") readonly FILE="${CMD}_v${VERSION}_Linux-64bit.tar.gz" ;;
-        esac
-        readonly URL="https://github.com/schollz/croc/releases/download/v${VERSION}/${FILE}"
-        wget -N "$URL"
-        tar -xvf "$FILE"
-        mv croc ~/.local/bin
-        rm -rf "$FILE"
-        rm -f README.md LICENSE
-        mkdir -p "$HOME/.config/bash_completion"
-        mkdir -p "$HOME/.config/zsh/completions"
-        mv -f bash_autocomplete "$HOME/.config/bash_completion/croc"
-        mv -f zsh_autocomplete "$HOME/.config/zsh/completions/_croc"
+        curl https://getcroc.schollz.com | bash
     fi
 fi
