ee26a39cf3b1aea35ee9961e7ff479fa1144f136
yamaton <yamaton@gmail.com>
2025-07-30 08:59:53 -0400

    Update nala installation in topgrade

--- _setup-topgrade.sh
+++ _setup-topgrade.sh
@@ -66,10 +66,7 @@ if [[ "${1-}" == "-f" ]] || [[ ! "$(command -v ${NAME})" ]] || [[ "$confirm" == 
             echo "--------------------------------------------"
             echo "Installing nala"
             echo "--------------------------------------------"
-            wget https://gitlab.com/volian/volian-archive/uploads/b20bd8237a9b20f5a82f461ed0704ad4/volian-archive-keyring_0.1.0_all.deb
-            wget https://gitlab.com/volian/volian-archive/uploads/d6b3a118de5384a0be2462905f7e4301/volian-archive-nala_0.1.0_all.deb
-            sudo apt install ./volian-archive-keyring_0.1.0_all.deb
-            sudo apt install ./volian-archive-nala_0.1.0_all.deb
+            curl https://gitlab.com/volian/volian-archive/-/raw/main/install-nala.sh | bash
             sudo apt update && sudo apt install nala
         fi
     fi
