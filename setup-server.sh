fced1e2c1af3ec2e7dd11a5f51d14bc883fdab2e
yamaton <yamaton@gmail.com>
2026-05-23 18:46:18 -0400

    Remove some default apps

--- setup-server.sh
+++ setup-server.sh
@@ -67,7 +67,7 @@ cd "${BASEDIR}" || exit
 
 
 # custom installations
-APPS=(tmux emacs neovim tealdeer cheatsheet ripgrep bat fd parallel nnn fzf broot delta topgrade zoxide yq hyperfine)
+APPS=(tmux emacs neovim tealdeer cheatsheet ripgrep bat fd parallel fzf delta topgrade zoxide yq hyperfine)
 for app in "${APPS[@]}"; do
     echo ""
     echo "--------------------------"
